d40384ce2c73037af260068ebb91d8ebdd413add
dutkyobot <dutkyobot@gmail.com>
2024-09-18 12:41:27 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,10 +1,1 @@
-npm install firebase
-
-import { initializeApp } from 'firebase/app';
-
-// TODO: Replace the following with your app's Firebase project configuration
-const firebaseConfig = {
-  //...
-};
-
-const app = initializeApp(firebaseConfig);
+npm install -g firebase-tools
